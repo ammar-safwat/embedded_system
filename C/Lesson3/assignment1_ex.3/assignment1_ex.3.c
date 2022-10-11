5a5ca738befd5486f682c8b8c38756641895318c
ammar safwat <ammar.badawy96@gmail.com>
2022-10-11 06:08:44 +0200

    assignment1-3

---
+++ C/Lesson3/assignment1_ex.3/assignment1_ex.3.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int x,y;
+	printf("enter two integers :");
+	fflush(stdin); fflush(stdout);
+	scanf("%d%d",&x,&y);
+	printf("sum = %d",x+y);
+
+}

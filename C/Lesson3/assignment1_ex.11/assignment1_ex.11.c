5647c287468d212f0d27b0f3bcc7bd2205dfe5cb
ammar safwat <ammar.badawy96@gmail.com>
2022-10-24 09:49:17 +0200

    assignment1_11

---
+++ C/Lesson3/assignment1_ex.11/assignment1_ex.11.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	char c;
+	printf("Enter a character: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%c", &c);
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+		printf("%c is an alphabet.", c);
+	}
+	else {
+		printf("%c is not an alphabet.", c);
+	}
+
+	return 0;
+}
